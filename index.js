d348633d1a0f46d0daebfb26b23ba285b0b2d029
Tikay <kiet211194@gmail.com>
2020-04-08 13:54:10 +0700

    Learn-template

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var low = require('lowdb');
+var bodyParser = require('body-parser');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+var shortid = require('shortid');
+
+db = low(adapter);
+
+db.defaults({ users: [] })
+  .write();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+app.get('/', function (req, res) {
+  res.render('index')
+});
+app.get('/users', function (req, res) {
+  res.render('users/index', {
+  	users: db.get('users').value()
+  })
+});
  
-app.get('/', function (request, respone) {
-  respone.send('Hello World')
-})
- 
+app.get('/users/search', function (req, res) {
+  var q = req.query.q;
+  var matchedUsers = db.get('users').filter(function(user){
+  	return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+  }).value();
+
+  res.render('users/index', {
+  	users: matchedUsers
+  })
+});
+
+app.get('/users/create', function (req, res) {
+  res.render('users/create');
+});
+
+app.get('/users/:id', function (req, res) {
+	var id = req.params.id;
+	var user = db.get('users').find({id: id}).value();
+	res.render('users/view', {
+		user: user
+	});
+});
+
+
+app.post('/users/create', function (req, res) {
+  req.body.id = shortid.generate();
+  db.get('users').push(req.body).write();
+  res.redirect('/users');
+});
+
+
 app.listen(3000, function(){
 	console.log('Hey men, server listening on port 3000')
 })
