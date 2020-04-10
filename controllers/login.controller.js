291340bf61c49da26a1b35cc957510b311fb90c6
Tikay <kiet211194@gmail.com>
2020-04-10 16:15:54 +0700

    login

---
+++ controllers/login.controller.js
@@ -0,0 +1,30 @@
+var db = require('../db.js');
+var shortid = require('shortid');
+
+module.exports.login = function (req, res) {
+  res.render('auth/login')
+};
+
+module.exports.postLogin = function (req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+  var user = db.get('users').find({ email: email}).value();
+
+  if (!user) {
+  	res.render('auth/login', {
+  		error : [
+  		'User was wrong'],
+  		values : req.body
+  	});
+    return;
+  }
+  if (user.password !== password){
+  	res.render('auth/login', {
+  		error : [
+  		'Password was wrong'],
+  		values : req.body
+  	});
+    return;
+  }
+  res.redirect('/users');
+}
--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 var express = require('express');
 var app = express();
+var cookieParser = require('cookie-parser');
 
 var bodyParser = require('body-parser');
 
-var userRoute = require('./routes/user.route')
+var userRoute = require('./routes/user.route');
+var authRoute = require('./routes/login.route')
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) ;// for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
 
 app.get('/', function (req, res) {
   res.render('index')
 });
 
 app.use('/users', userRoute);
-
+app.use('/auth', authRoute);
 
 app.listen(3000, function(){
 	console.log('Hey men, server listening on port 3000')
